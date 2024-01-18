96baf2eec32e0a9cea2eda452455468b94c6623d
Amritdeep Singh <amsingh@fastenal.com>
2024-01-18 14:57:50 +0530

    Fixed db closing issue

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -3,12 +3,15 @@ const router = express.Router();
 
 const sqlite = require("sqlite3").verbose();
 
-const db = new sqlite.Database("./test.db", sqlite.OPEN_READWRITE, (err) => {
-  if (err) return console.error(err);
-});
-
 router.get("/", (req, res) => {
   try {
+    const db = new sqlite.Database(
+      "./test.db",
+      sqlite.OPEN_READWRITE,
+      (err) => {
+        if (err) return console.error(err);
+      }
+    );
     db.all(`select * from courses`, [], (err, rows) => {
       if (err) return res.json({ success: false, status: 400, msg: "hello" });
 
