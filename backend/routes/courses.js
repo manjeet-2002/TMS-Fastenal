faaa0ebb787d71a321096f8f6430e134b0d6424a
amrit <amsingh@fastenal.com>
2024-01-22 15:23:13 +0530

    Added functionality to add A new course Also changed c_id from integer to text

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -23,7 +23,7 @@ module.exports = (db) => {
 
   router.get("/:c_id", (req, res) => {
     try {
-      const c_id = parseInt(req.params.c_id);
+      const c_id = req.params.c_id;
 
       db.get(`SELECT * FROM courses WHERE c_id=(?)`, [c_id], (err, course) => {
         console.log(course);
@@ -39,27 +39,47 @@ module.exports = (db) => {
     }
   });
 
-  //-----------POST A COURSE---------------
+  //-----------POST A COURSE AND RESPECTIVE MODULES---------------
 
   router.post("/", (req, res) => {
     try {
       const {
-        c_id,
-        c_name,
-        start_date,
-        end_date,
-        duration,
-        credits,
-        max_attendees,
+        courseId: c_id,
+        courseName: c_name,
+        startDate: start_date,
+        endDate: end_date,
+        courseDuration: duration,
+        courseCredits: credits,
+        maxAttendees: max_attendees,
+        dynamicList: modules,
       } = req.body;
-
+      console.log(c_id, modules, c_name);
       db.run(
         `INSERT INTO courses VALUES(?,?,?,?,?,?,?)`,
-        [c_id, c_name, start_date, end_date, duration, credits, max_attendees],
+        [
+          c_id,
+          c_name,
+          start_date,
+          end_date,
+          parseInt(duration),
+          parseInt(credits),
+          parseInt(max_attendees),
+        ],
         (err) => {
           if (err) return console.error(err);
         }
       );
+      modules.forEach((module) => {
+        console.log(module);
+        db.run(
+          `INSERT INTO modules VALUES (?,?,?)`,
+          [module.m_id, module.name, c_id],
+          (err) => {
+            if (err) return console.error(err);
+          }
+        );
+      });
+
       res.json({
         status: 201,
         success: true,
@@ -71,3 +91,35 @@ module.exports = (db) => {
 
   return router;
 };
+//   router.post("/", (req, res) => {
+//     try {
+//       const {
+//         c_id,
+//         c_name,
+//         start_date,
+//         end_date,
+//         duration,
+//         credits,
+//         max_attendees,
+//       } = req.body;
+
+//       db.run(
+//         `INSERT INTO courses VALUES(?,?,?,?,?,?,?)`,
+//         [c_id, c_name, start_date, end_date, duration, credits, max_attendees],
+//         (err) => {
+//           if (err) return console.error(err);
+//         }
+//       );
+//       res.json({
+//         status: 201,
+//         success: true,
+//       });
+//     } catch (err) {
+//       if (err) return res.json({ success: false, status: 400 });
+//     }
+//   });
+
+//   return router;
+// };
+
+//--- POSTING A NEW COURSE
